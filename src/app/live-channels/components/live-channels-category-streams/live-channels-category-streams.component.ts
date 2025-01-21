e6e05385a95043b959c2c0735b8c38910ec3e0f1
Nader Magdy <Nader.Magdy@integrant.com>
2025-01-21 16:34:08 +0200

    Show channels play list

--- src/app/live-channels/components/live-channels-category-streams/live-channels-category-streams.component.ts
+++ src/app/live-channels/components/live-channels-category-streams/live-channels-category-streams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Stream } from 'src/app/models/stream.model';
@@ -12,16 +13,33 @@ import { LiveChannelsService } from 'src/app/services/live-channels.service';
 })
 export class LiveChannelsCategoryStreamsComponent implements OnInit {
   streams$: Observable<Stream[]> | undefined;
-  playerSource : string = '';
+  playerSource: string = '';
+  playlistUrl: SafeResourceUrl = '';
   constructor(private liveChannelsService: LiveChannelsService,
-    private readonly _hostingService : HostingService,
+    private readonly _hostingService: HostingService,
+    private sanitizer: DomSanitizer,
     private route: ActivatedRoute) {
-      this.playerSource = this._hostingService.getStreamSourceBase();
+    this.playerSource = this._hostingService.getStreamSourceBase();
   }
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       const categoryId = params['category-id'];
       this.streams$ = this.liveChannelsService.getStreams(categoryId);
+      this.streams$.subscribe((streams) => {
+        this.buildPlaylist(streams);
+      })
     });
+  }
+  buildPlaylist(streams: Stream[]) {
+    const data =  '#EXTM3U' + streams.map(s => this.streamToM3U(s)).join('');
+    const blob = new Blob([data], { type: 'application/vnd' });
+
+    this.playlistUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+  }
+  streamToM3U(stream: Stream) {
+    return `
+    #EXTINF:0,${stream.stream_id} - ${stream.name}
+    #EXTVLCOPT:network-caching=1000
+    ${this.playerSource}/${stream.stream_id}`
   }
 }
